7103f596456269a0f4565883500cfcd32f0714b6
Suman Pal <suman7495@gmail.com>
2019-03-12 15:52:47 +0100

    Update table_env.py

--- gym_table/envs/table_env.py
+++ gym_table/envs/table_env.py
@@ -1,1 +1,21 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
 
+class FooEnv(gym.Env):
+  metadata = {'render.modes': ['human']}
+
+  def __init__(self):
+    print("In init")
+    
+  def step(self, action):
+    return
+  
+  def reset(self):
+    return
+  
+  def render(self, mode='human'):
+    return 
+  
+  def close(self):
+    return
